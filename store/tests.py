a50cf21319e6326bc7330a756886765ba6fe0b53
Thomás César <thomascesar000@gmail.com>
2026-08-29 21:31:46 -0300

    test: adiciona testes basicos da api

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+class CategoryTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='teste',
+            password='12345678'
+        )
+
+        self.client.force_authenticate(
+            user=self.user
+        )
+
+    def test_criar_categoria(self):
+
+        response = self.client.post(
+            '/api/categories/',
+            {
+                'name': 'Eletronicos',
+                'description': 'Produtos eletronicos'
+            },
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_listar_categorias(self):
+
+        response = self.client.get(
+            '/api/categories/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
